5de6df520a36e6d80fe4795a016c148a6981176b
Michael Strand <32778224+MichaelLykkeStrand@users.noreply.github.com>
2022-02-06 04:12:40 +0100

    Update checkAdmin.mjs

--- middleware/checkAdmin.mjs
+++ middleware/checkAdmin.mjs
@@ -3,10 +3,8 @@ import UserProfile from "../models/UserProfile.mjs"
 export default async (req, res, next) => {
   try {
     const user = await UserProfile.findOne({username: req.user.username});
-    if (!user) {
-      return res.status(400).json({error: 'invalid session'});
-    }
-
+    if (!user) return res.status(400).json({error: 'invalid session'});
+    
     const userHasRole = user.userRoles.includes('admin');
     if (!userHasRole) return res.status(401).json({error: 'unauthorized'});
 
